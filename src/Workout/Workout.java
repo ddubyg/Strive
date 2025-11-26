c95111ca9154f2cdb756643dbc0b2d63228f665a
renzo1417 <ensoo1713@gmail.com>
2025-11-26 23:53:47 +0800

    workout cardio

--- src/Workout/Workout.java
+++ src/Workout/Workout.java
@@ -6,11 +6,34 @@ public class Workout extends JFrame {
 
 
     private JPanel mainPanel;
+    private JPanel titlePanel;
+    private JPanel lowerPanel;
+    private JButton CARDIOButton;
+    private JButton STRENGTHButton;
+    private JPanel cardioPanel;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JPanel durationPanel;
+    private JSpinner spinner1;
+    private JTextField textField3;
+    private JRadioButton maleRadioButton;
+    private JRadioButton femaleRadioButton;
+    private JPanel belowPanel;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton CALCULATEButton;
+    private JTextField textField6;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton routineButton;
 
     public Workout(){
         setContentPane(mainPanel);
-        setSize(402, 874);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(402, 874);
         setVisible(true);
 
     }
