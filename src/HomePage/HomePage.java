f21d4477ece490fd10c326e77b45f0990e2f8011
froilan <johannefroilan@gmail.com>
2025-11-26 23:53:10 +0800

    ui homepage

--- src/HomePage/HomePage.java
+++ src/HomePage/HomePage.java
@@ -1,7 +1,5 @@
 package HomePage;
 
-import JournalPanel.Journal;
-
 import javax.swing.*;
 
 public class HomePage extends JFrame {
@@ -9,6 +7,25 @@ public class HomePage extends JFrame {
     private JPanel header;
     private JLabel titleLabel;
     private JPanel titlePanel;
+    private JPanel addJournalPanel;
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+    private JTextArea textArea3;
+    private JTextArea textArea4;
+    private JTextArea textArea5;
+    private JTextArea textArea6;
+    private JTextArea textArea7;
+    private JTextArea textArea8;
+    private JButton readyToStartYourButton;
+    private JButton browseExerciseButton;
+    private JButton CANCELButton;
+    private JButton STARTButton;
+    private JPanel lowerPanel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton routineButton;
 
     public HomePage(){
         setContentPane(mainPanel);
