edafae5872c2249b0bea63e67e3292b89c8c0ae9
nathanaelandrewmendoza <mnathanaelandrew@gmail.com>
2025-11-26 22:59:23 +0800

    strength page

--- src/Workout/StrengthPanel.java
+++ src/Workout/StrengthPanel.java
@@ -9,4 +9,13 @@ public class StrengthPanel {
     private JPanel optionPanel;
     private JButton cardioBtn;
     private JButton strengthBtn;
+    private JPanel exerciseNamePanel;
+    private JComboBox nameDropDown;
+    private JTextField setsTextField;
+    private JTextField repsTextField;
+    private JComboBox typeDropDown;
+    private JLabel repsLabel;
+    private JLabel typeLabel;
+    private JLabel setLabel;
+    private JLabel nameLabel;
 }
