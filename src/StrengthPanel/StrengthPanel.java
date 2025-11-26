b5c4543248b0042327e17186fdec9e9586479f63
nathanaelandrewmendoza <mnathanaelandrew@gmail.com>
2025-11-26 22:22:18 +0800

    strength page

---
+++ src/StrengthPanel/StrengthPanel.java
@@ -0,0 +1,12 @@
+package StrengthPanel;
+
+import javax.swing.*;
+
+public class StrengthPanel {
+    private JPanel strengthPanel;
+    private JPanel header;
+    private JLabel titleLabel;
+    private JPanel optionPanel;
+    private JButton cardioBtn;
+    private JButton strengthBtn;
+}
