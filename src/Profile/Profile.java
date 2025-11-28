067f3b6594e13759159ea93945aa3093d1dad634
renzo1417 <ensoo1713@gmail.com>
2025-11-28 22:44:07 +0800

    New profile gui, i change lang ang text for user like if mag input na sa log in ma savve ang user also for BMI

--- src/Profile/Profile.java
+++ src/Profile/Profile.java
@@ -1,90 +1,47 @@
 package Profile;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Profile extends JFrame {
 
     private JPanel mainFrame;
-    private JButton HOMEButton;
-    private JButton PROFILEButton;
-    private JButton WORKOUTButton;
-    private JButton JOURNALButton;
-    private JButton TARGETButton;
-    private JLabel strive_icon;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton routineButton;
+    private JProgressBar progressBarWeight;
+    private JButton EDITPROFILEButton;
+    private JTextField ageTextField;
+    private JTextField heightTextField;
+    private JTextField weightProfileTextField;
+    private JTextField goalTextField;
+    private JTextField monthlyLogsTextField;
+    private JPanel BMIpanel;
+    private JLabel userNameLabel;
+    private JPanel headPanel;
+    private JPanel userPanel;
+    private JPanel lowerPanel;
+    private JPanel editablePanel;
+    private JLabel editableJlabelBmi;
     private JButton EDITButton;
     private JButton confirm_button;
     private JPanel profile_panel;
     private JPanel edit_panel;
-    private JTextField textField1;
-    private JTextField cTextField;
-    private JComboBox comboBox1;
     private JLabel title_Label;
-
-    private CardLayout cardLayout;
-    private int currentIndex = 0;
-    private JPanel[] panels;
 
     public Profile(){
         setContentPane(mainFrame);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(402,874);
-        setTitle("Strive");
-
-        Panels();
-        listeners();
-
         setVisible(true);
 
     }
 
-    public void listeners(){
 
-        EDITButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(currentIndex < panels.length - 1){
-                    currentIndex++;
-                    cardLayout.next(mainFrame);
-                    updateButtonState();
-                }
-            }
-        });
-
-        confirm_button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(currentIndex > 0){
-                    currentIndex--;
-                    cardLayout.previous(mainFrame);
-                    updateButtonState();
-                }
-            }
-
-        });
-    }
-
-    public void Panels(){
-        cardLayout = new CardLayout();
-        mainFrame.setLayout(cardLayout);
-
-        panels = new JPanel[]{profile_panel, edit_panel};
-
-        mainFrame.add(profile_panel,"profile_panel");
-        mainFrame.add(edit_panel,"edit_panel");
-
-        cardLayout.show(mainFrame, "profile_panel");
-        updateButtonState();
-    }
-
-    private void updateButtonState() {
-        confirm_button.setEnabled(currentIndex > 0);
-        EDITButton.setEnabled(currentIndex < panels.length - 1);
-    }
 
     public static void main(String[] args) {
+
         new Profile();
     }
 
