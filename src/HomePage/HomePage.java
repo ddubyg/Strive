612f8edee2193aa60451525ac0b9e0ea19f4cfe3
renzo1417 <ensoo1713@gmail.com>
2025-11-30 23:07:35 +0800

    added streak, og new UI sa homepage og journal

--- src/HomePage/HomePage.java
+++ src/HomePage/HomePage.java
@@ -1,11 +1,11 @@
 package HomePage;
 
-import JournalPanel.Journal;
-
 import LoginForm.UserStore;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.temporal.WeekFields;
 
 
 public class HomePage extends JFrame {
@@ -21,8 +21,7 @@ public class HomePage extends JFrame {
     private JCheckBox fridayCheckBox;
     private JCheckBox saturdayCheckBox;
     private JCheckBox sundayCheckBox;
-    private JButton browseWorkoutButton;
-    private JButton CANCELButton;
+    private JButton BROWSEWORKOUTButton;
     private JButton STARTButton;
     public JButton home;
     public JButton workout;
@@ -30,11 +29,14 @@ public class HomePage extends JFrame {
     public JButton profile;
     public JButton routineButton;
     private JLabel welcome;
-
+    private JLabel streakLabel;
+    private JLabel displayCaloriesJlabel;
+    private int weeklyStreak = 0;
+    private boolean hasCountedThisWeek = false;
+    private int lastWeekNumber = -1;
 
     public HomePage(){
-
-
+        setStreakLabel();
 
     }
     //setting welcome
@@ -45,6 +47,37 @@ public class HomePage extends JFrame {
 
     public JPanel getMainPanel() {
         return mainPanel;
+    }
+
+
+    public void setStreakLabel(){
+        STARTButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LocalDate today = LocalDate.now();
+                int currentWeek = today.get(WeekFields.ISO.weekOfYear());
+
+                if (currentWeek != lastWeekNumber) {
+                    hasCountedThisWeek = false;
+                    lastWeekNumber = currentWeek;
+                }
+                boolean selected =
+                        mondayCheckBox.isSelected() ||
+                                tuesdayCheckBox.isSelected() ||
+                                wednesdayCheckBox.isSelected() ||
+                                thursdayCheckBox.isSelected() ||
+                                fridayCheckBox.isSelected() ||
+                                saturdayCheckBox.isSelected() ||
+                                sundayCheckBox.isSelected();
+
+                if (selected && !hasCountedThisWeek) {
+                    weeklyStreak++;
+                    hasCountedThisWeek = true;
+                }
+                streakLabel.setText(String.valueOf(weeklyStreak));
+
+            }
+        });
     }
 
 
