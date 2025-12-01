20fb1649114905441ac9c58114367411cf05db11
LAB15_USER ON WS18 <L15X15W18@LabHCI.net>
2025-12-01 15:59:06 +0800

    try-catch age, height, weight

--- src/LoginForm/CreateAccount.java
+++ src/LoginForm/CreateAccount.java
@@ -5,7 +5,7 @@ import Main.PageManager;
 import javax.swing.*;
 import java.awt.event.ActionListener;
 
-public class CreateAccount{
+public class CreateAccount {
     private PageManager manager;
     private JPanel mainPanel;
     private JPanel createAccountPanel;
@@ -19,29 +19,59 @@ public class CreateAccount{
     private JTextField weightTextfield;
     public JButton CREATEPROFILEButton;
 
-    public CreateAccount(PageManager manager){
+    public CreateAccount(PageManager manager) {
         this.manager = manager;
-    setProfile();
+        setProfile();
     }
-// para sa log in and to store some info for profile
-    public void setProfile(){
-        CREATEPROFILEButton.addActionListener(e ->{
-            UserStore.username = userTextfield.getText();
-            UserStore.password = passwordTextfield.getText();
-            UserStore.displayName = displayNameTExtfield.getText().toUpperCase();
-            UserStore.age = ageTextField.getText();
-            UserStore.height = heightTextField.getText();
-            UserStore.weight = weightTextfield.getText();
-            JOptionPane.showMessageDialog(null, "Account created!");
-            userTextfield.setText("");
-            passwordTextfield.setText("");
-            displayNameTExtfield.setText("");
-            ageTextField.setText("");
-            heightTextField.setText("");
-            weightTextfield.setText("");
 
-            manager.showPage("login");
+    // This method is responsible for handling the profile creation process
+    public void setProfile() {
+        CREATEPROFILEButton.addActionListener(e -> {
+            try {
+                // Validate and parse age as integer
+                int age = Integer.parseInt(ageTextField.getText());
+                if (age <= 0) {
+                    throw new NumberFormatException("Age must be a positive integer.");
+                }
+                UserStore.age = String.valueOf(age);
 
+                // Validate and parse height as double
+                double height = Double.parseDouble(heightTextField.getText());
+                if (height <= 0) {
+                    throw new NumberFormatException("Height must be a positive number.");
+                }
+                UserStore.height = String.valueOf(height);
+
+                // Validate and parse weight as double
+                double weight = Double.parseDouble(weightTextfield.getText());
+                if (weight <= 0) {
+                    throw new NumberFormatException("Weight must be a positive number.");
+                }
+                UserStore.weight = String.valueOf(weight);
+
+                // Store other user data
+                UserStore.username = userTextfield.getText();
+                UserStore.password = passwordTextfield.getText();
+                UserStore.displayName = displayNameTExtfield.getText().toUpperCase();
+
+                // Show success message
+                JOptionPane.showMessageDialog(null, "Account created!");
+
+                // Clear the input fields
+                userTextfield.setText("");
+                passwordTextfield.setText("");
+                displayNameTExtfield.setText("");
+                ageTextField.setText("");
+                heightTextField.setText("");
+                weightTextfield.setText("");
+
+                // Navigate to login page
+                manager.showPage("login");
+
+            } catch (NumberFormatException ex) {
+                // Show error message in case of invalid input
+                JOptionPane.showMessageDialog(null, "Invalid input: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            }
         });
     }
 
